1bd7dac3abe2f663e205582dbd5b8b810405994a
DannyVFilms <44555970+dannyvfilms@users.noreply.github.com>
2023-03-29 12:04:58 -0500

    Update gmailsummary.py

--- gmailsummary.py
+++ gmailsummary.py
@@ -49,6 +49,11 @@ if not creds or not creds.valid:
     else:
         flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
+        
+        # Print the authorization URL
+        auth_url, _ = flow.authorization_url(prompt='consent')
+        print(f"Please visit this URL to authorize the app: {auth_url}", flush=True)
+        
 
     with open('token.pickle', 'wb') as token:
         pickle.dump(creds, token)
@@ -70,50 +75,99 @@ def fetch_latest_emails():
     latest_emails = []   
 
     # Fetch the full message details and extract label IDs and message headers
-    for message in messages:   
-        msg = gmail_service.users().messages().get(userId='me', id=message['id'], format='full').execute()   
+    for message in messages:
+        msg = gmail_service.users().messages().get(userId='me', id=message['id'], format='full').execute()
         labels = msg.get("labelIds", [])
         headers = msg['payload']['headers']
 
-        # Check if the message is unread, and extract the payload data and message parts
-        if 'UNREAD' in labels:   
-            payload = msg['payload']   
-            parts = payload.get("parts")   
+        if 'UNREAD' in labels:
+            payload = msg['payload']
+            parts = payload.get("parts")
+            decoded_data = None
+
+            # Extract the subject from the headers before processing the parts
+            subject = next((header['value'] for header in headers if header['name'] == 'Subject'), 'No Subject')
+
+            # Initialize decoded_data variable for each message
+            decoded_data = None
 
             # Extract the data from the first part, or the payload itself if there are no parts
-            data = parts[0] if parts else payload   
-            file_data = data.get("body", {}).get("data")   
-            
-            # Initializes an empty list to store the latest emails.
+            if parts:
+                for part in parts:
+                    content_type = part.get("mimeType", "")
+                    file_data = part.get("body", {}).get("data")
+
+                    if file_data:
+                        file_data = file_data.replace("-", "+").replace("_", "/")
+                        decoded_file_data = base64.b64decode(file_data).decode("utf-8")
+
+                    # If 'multipart/alternative' content type is found, process the subparts
+                    if 'multipart/alternative' in content_type:
+                        sub_parts = part.get("parts", [])
+                        for sub_part in sub_parts:
+                            sub_content_type = sub_part.get("mimeType", "")
+                            if 'text/plain' in sub_content_type:
+                                file_data = sub_part.get("body", {}).get("data")
+                                if file_data:
+                                    file_data = file_data.replace("-", "+").replace("_", "/")
+                                    decoded_data = base64.b64decode(file_data).decode("utf-8")
+                                    break
+                    elif 'text/calendar' in content_type:
+                        decoded_data = decoded_file_data
+                        break
+            else:
+                file_data = payload.get("body", {}).get("data")
+                if file_data:
+                    file_data = file_data.replace("-", "+").replace("_", "/")
+                    decoded_data = base64.b64decode(file_data).decode("utf-8")
+
             if file_data:   
                 file_data = file_data.replace("-", "+").replace("_", "/")   
                 decoded_data = base64.b64decode(file_data)   
                 if isinstance(decoded_data, bytes):   
                     decoded_data = decoded_data.decode("utf-8")   
-           
-            # If the message body is empty or contains no content, set a default value
-            if not decoded_data:
-                decoded_data = "No content"
-  
 
             # Check if the message is a calendar event
-            content_type = next((header['value'] for header in headers if header['name'] == 'Content-Type'), '')
-            if 'text/calendar' in content_type:
-                sender = 'Calendar'
-                event_details = decoded_data
+            sender = next((header['value'] for header in headers if header['name'] == 'From'), 'Unknown Sender')
+            is_calendar_event = False
+            event_details = ""
+
+            if parts:
+                for part in parts:
+                    content_type = part.get("mimeType", "")
+                    if 'text/calendar' in content_type:
+                        is_calendar_event = True
+                        file_data = part.get("body", {}).get("data")
+                        break
             else:
-                sender = next((header['value'] for header in headers if header['name'] == 'From'), 'Unknown Sender')
+                content_type = payload.get("mimeType", "")
+                if 'text/calendar' in content_type:
+                    is_calendar_event = True
+                    file_data = payload.get("body", {}).get("data")
+
+            if is_calendar_event:
+                sender = 'Calendar'
+
+                if file_data:
+                    file_data = file_data.replace("-", "+").replace("_", "/")
+                    decoded_data = base64.b64decode(file_data)
+                    event_details = decoded_data.decode('utf-8') if isinstance(decoded_data, bytes) else decoded_data
+            else:
+                # If the message body is empty or contains no content, set a default value
+                if not decoded_data:
+                    decoded_data = "No content"
                 event_details = decoded_data
 
+
             # Create a dictionary with email details
-            mail_data = {   
+            mail_data = {
                 'id': msg['id'],
-                'subject': next((header['value'] for header in headers if header['name'] == 'subject'), 'No Subject'),
+                'subject': subject,  # Use the extracted subject
                 'from': sender,
                 'body': event_details,
                 'internalDate': int(msg['internalDate'])
             }
-            
+
             # Add the email details to the latest emails list, stopping the loop if the maximum number of emails has been fetched
             latest_emails.append(mail_data)   
             if len(latest_emails) >= 10:
@@ -162,7 +216,7 @@ def remove_html_and_links(text):
 
     # Remove CSS styles
     text = re.sub(r'<style.*?>.*?</style>', '', text, flags=re.DOTALL)
-    text = re.sub(r'<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', '', text, flags=re.DOTALL) # Added
+    text = re.sub(r'<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', '', text, flags=re.DOTALL)
 
     # Remove JavaScript code
     text = re.sub(r'<script.*?>.*?</script>', '', text, flags=re.DOTALL)
@@ -200,7 +254,7 @@ def get_emails_summary():
     email_ids = email_ids_string.replace('ids[]=', '').strip('"').split('","')
 
     # Print the received email IDs
-    print(f"Received email IDs: {email_ids}")
+    print(f"Received email IDs: {email_ids}", flush=True)
 
     # Find the emails with the given ids in the latest_emails list
     emails = [e for e in latest_emails if e['id'] in email_ids and e.get('subject', '') != 'No subject' and e.get('content', '') != 'No content']
@@ -217,8 +271,8 @@ def get_emails_summary():
     prompt = f"{CUSTOM_PROMPT}\n\n{email_content}"
 
     # Print the prompt to the console
-    print("\n" + "#" * 45 + "\n    Generated Prompt:   \n" + "#" * 45 + "\n")
-    print(prompt)
+    print("\n" + "#" * 45 + "\n    Generated Prompt:   \n" + "#" * 45 + "\n", flush=True)
+    print(prompt, flush=True)
 
     # Generate a summary of the emails using the OpenAI API
     headers = {
@@ -245,42 +299,43 @@ def get_emails_summary():
         response.raise_for_status()
         response_json = response.json()
     except requests.exceptions.RequestException as e:
-        print(f"Error connecting to OpenAI API: {e}")
+        print(f"Error connecting to OpenAI API: {e}", flush=True)
         return jsonify({"error": "Failed to connect to OpenAI API."})
     except ValueError as e:
-        print(f"Error parsing OpenAI API response: {e}")
+        print(f"Error parsing OpenAI API response: {e}", flush=True)
         return jsonify({"error": "Failed to parse OpenAI API response."})
 
     try:
         response_json = response.json()
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Error: {e}", flush=True)
         return jsonify({"error": str(e)})
 
-    print("\n" + "#" * 45 + "\n    OpenAI Response Payload:   \n" + "#" * 45 + "\n")
-    print(response_json)
+    print("\n" + "#" * 45 + "\n    OpenAI Response Payload:   \n" + "#" * 45 + "\n", flush=True)
+    print(response_json, flush=True)
 
     if 'error' in response_json:
-        print(f"OpenAI API error: {response_json['error']['message']}")
+        print(f"OpenAI API error: {response_json['error']['message']}", flush=True)
         return jsonify({"error": response_json["error"]["message"]})
 
     try:
         summary = response_json["choices"][0]["message"]["content"].strip()
     except KeyError:
-        print("Error extracting summary from OpenAI API response.")
+        print("Error extracting summary from OpenAI API response.", flush=True)
         return jsonify({"error": "Failed to extract summary from OpenAI API response."})
     
     # Mark the emails as read
-    print("\n" + "#" * 45 + "\n    Marking emails as read.   \n" + "#" * 45 + "\n")
+    print("\n" + "#" * 45 + "\n    Marking emails as read.   \n" + "#" * 45 + "\n", flush=True)
     try:
         mark_emails_read(email_ids)
     except Exception as e:
-        print(f"Error marking emails as read: {e}")
+        print(f"Error marking emails as read: {e}", flush=True)
 
     # Return the summary as plain text instead of JSON
-    print("\n" + "#" * 45 + "\n    OpenAI Response:   \n" + "#" * 45 + "\n")
-    print(summary)
+    print("\n" + "#" * 45 + "\n    OpenAI Response:   \n" + "#" * 45 + "\n", flush=True)
+    print(summary, flush=True)
     return jsonify({"summary": summary})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1337)
+
